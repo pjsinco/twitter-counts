0aa8f86973e0a3ed844fe0aac2fc7dcf55fd0c62
pjsinco <psinco@fas.harvard.edu>
2014-07-15 10:42:54 -0500

    adds connection test

--- get-twitter-stats.php
+++ get-twitter-stats.php
@@ -6,3 +6,5 @@ require_once('config/config.php');
 
 $db = new PDO('mysql:host=' . DB_HOST . ';db=' .  DB_NAME . 
   ';port=' . DB_PORT, DB_USER, DB_PASS);
+
+echo ($db) ? 'connected' : 'didn\'t connect';
