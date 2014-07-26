127de348a384ecc468fc45abd4579ee600d27997
Patrick Sinco <psinco@gmail.com>
2014-07-25 21:08:51 -0500

    adds update_all_account_profiles.php

---
+++ scripts/update_all_account_profiles.php
@@ -0,0 +1,11 @@
+<?php 
+
+$q = "
+  SELECT user_id
+  FROM tc_user
+  WHERE last_update < date_sub(now(), interval 24 hour)
+  limit 15000
+";
+
+require 'collect_account_profiles.php';
+collect_account_profiles($q);
